7d87554cb0c1c7d9f08df01bb21513c20e84980f
Zac Brachmanis <zbrachmanis@san-mac4119.merchant.local>
2015-08-25 17:58:03 -0700

    Initial Commit

---
+++ persuasion_analyzer.py
@@ -0,0 +1,35 @@
+from subprocess import call
+
+def getNGrams(n,inputFile):
+    
+    text = open(inputFile, 'r')
+
+    count = len(open(inputFile, 'r').read().lower().strip().split())
+
+    grams = {}
+
+    for line in text: 
+        
+        myArr = line.lower().strip().split()
+        
+        size = len(myArr) - ( n - 1 )
+
+        for x in range(0, size):
+            
+            gram = []
+
+            for y in range(0,n):
+                gram.append(myArr[x + y])
+
+            if '-'.join(gram) in grams:
+                grams['-'.join(gram)] += 1
+            else:
+                grams['-'.join(gram)] = 1
+
+    return grams
+
+trigrams = getNGrams(3,'persuasion.txt')
+
+for x in trigrams:
+    
+    print str(x) + ': ' + str(trigrams[x])
