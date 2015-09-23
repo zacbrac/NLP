a627fcfd1fb09645e3e1914ec83ce96a466d6af3
Zachary Brachmanis <zac@kodigosystems.com>
2015-09-23 15:18:45 -0700

    Imported more books

--- analyzer.py
+++ analyzer.py
@@ -1,13 +1,22 @@
-# import os
+from os import listdir
+from os.path import isfile, join
 import json
 from Grammer import Grammer
 
-os.system('clear')
+mypath = './books/'
+book_filename = 'king_james.txt'
+
+# Gets all books in books/ 
+# onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
+
+# for book in onlyfiles:
+
 
 Grammer = Grammer()
 
-text = raw_input('What text would you like to validate: ')
-print Grammer.checkStringForCorrectness(text)
+# text = raw_input('What text would you like to validate: ')
+# print Grammer.checkStringForCorrectness(text)
 
+# 10 times to get 1-10grams for each book
 # for x in range(10,0,-1):
-#     print Grammer.importNGrams(x, 'books/the_new_hackers_dictionary.txt')
+#     print Grammer.importNGrams(x, mypath + book_filename)
